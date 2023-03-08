0862843f8c27e89ca90f75183547d57b99020c95
hectoru2001 <122303610+hectoru2001@users.noreply.github.com>
2023-03-08 12:35:32 -0700

    Se actalizó a una nueva version

--- Finanzas.py
+++ Finanzas.py
@@ -1,9 +1,11 @@
 import os
+import mysql.connector
 os.system("cls")
 
-dCant = 0
-dGast = 0
-
+conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="", database="user")
+eject = conexion1.cursor()
+subtotal = 0
+total = 0
 while True:
     print("Menú de opciones:")
     print("1. Ingresar gasto")
@@ -15,29 +17,44 @@ while True:
     opcion = input("Ingrese una opción: ")
     
     if opcion == "1":
-        bddGast = open("dGast.txt", "w")
-        dGast = input("Ingresa la cantidad que se haya gastado $")
-        bddGast.write(dGast)
-        input("")
-        bddGast.close()
+        gas = int(input("Ingrese un Gasto: "))
+        mot = input('Ingrese el motivo del gasto: ')
+        sql = "INSERT INTO ingresos (gasto, motivo) VALUES (%s, %s)"
+        values = (gas, mot)
+        total = (subtotal - gas)
+        eject.execute(sql, values)
+        conexion1.commit()
+        print(eject.rowcount, input("Valor insertado correctamente, pulsa cualquier tecla para continuar " + str(total)))
+        os.system("cls")
     elif opcion == "2":
-        input("")
+        os.system("cls")
+        print("Esta es la cantidad de dinero con la que cuentas actualmente: " + str(total))
+        input("Pulsa cualquier tecla para continuar")
+        os.system("cls")
     elif opcion == "3":
-        bddCant = open("dCant.txt", "w")
-        dCant = input("Ingresa la cantidad que deseas cambiar $")
-        bddCant.write(dCant)
-        bddCant.close()
-        input("")
+        os.system("cls")
+        ncant = input("Ingrese la nueva cantidad de dinero ")
+        total = ncant        
+        print("Esta es la cantidad de dinero con la que cuentas actualmente: " + str(total))
+        input("Pulsa cualquier tecla para continuar")
+        os.system("cls")
     elif opcion == "4":
-        bddIngr = open("dIngr.txt", "w")
-        dIngr = input("Ingresa la cantidad cantidad que se haya ingresado $")
-        bddIngr.write(dIngr)
-        print("Esta es la cantidad de dinero con la que cuentas actualmente $")
-        bddIngr.close()
-        input("")
+        ing = int(input("Ingrese un cantidad de dinero: "))
+        mot = input('Ingrese el motivo del gasto: ')
+        sql = "INSERT INTO ingresos (ingreso, motivo) VALUES (%s, %s)"
+        values = (ing, mot)
+        eject.execute(sql, values)
+        conexion1.commit()
+        print(eject.rowcount, "record inserted.")
     elif opcion == "5":
-        print("Hasta la próxima")
+        os.system("cls")
+        print("Nos vemos pronto")
         break
+    elif opcion == "6":
+        eject.execute("SELECT * FROM ingresos")
+        resultados = eject.fetchall()
+        for fila in resultados:
+            print(fila)
     else:
         print("Opción inválida. Por favor, seleccione una opción válida.")
         input("Presiona enter para continuar")
