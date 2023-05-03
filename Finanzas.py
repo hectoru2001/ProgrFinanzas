bdd4c487f6e6f2c927084e977c1c9bdec2878260
hectoru2001 <122303610+hectoru2001@users.noreply.github.com>
2023-05-03 03:59:33 -0600

    Se actualizó la versión del programa
    Se ha actualizado el programa a una versión donde funciona correctamente

--- Finanzas.py
+++ Finanzas.py
@@ -1,60 +1,90 @@
-import os
-import mysql.connector
-os.system("cls")
+import os, mysql.connector, datetime
 
-conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="", database="user")
-eject = conexion1.cursor()
-subtotal = 0
-total = 0
+# Conexión con la base de datos
+conexion = mysql.connector.connect(host="localhost", user="root", passwd="", database="user")
+ejecucion = conexion.cursor()
+
+
 while True:
-    print("Menú de opciones:")
-    print("1. Ingresar gasto")
-    print("2. Mostrar dinero actual")
-    print("3. Ingresar nueva cantidad de dinero")
-    print("4. Ingresar un nuevo déposito")
-    print("5. Salir del programa")
+    # Limpiar consola
+    os.system("cls")
+
+    # Extraer el total de la base de datos
+    ejecucion.execute("SELECT total FROM ingresos ORDER BY id DESC LIMIT 1")
+    resultado = ejecucion.fetchone()
+    if resultado == None:
+        print("Error al encontrar información en la base de datos")
+        total = 0
+    else:
+        total = int(resultado[0])
     
-    opcion = input("Ingrese una opción: ")
+    print("----- Bienvenido al programa de finanzas -----")
+    print("")
+    print("- Por favor elige la transacción que desees realizar -")
+    print("1. Mostrar mi cantidad actual de dinero")
+    print("2. Registrar un nuevo gasto de dinero")
+    print("3. Registrar un nuevo ingreso de dinero")
+    print("4. Cambiar la cantidad del total por una nueva")
+    print("5. Mostrar los últimos gastos realizados")
+    print("6. Mostrar los últimos ingresos realizados")
+    print("7. Eliminar historial")
+
+
+
+
+    print()
+    opcion = input()
     
     if opcion == "1":
-        gas = int(input("Ingrese un Gasto: "))
-        mot = input('Ingrese el motivo del gasto: ')
-        sql = "INSERT INTO ingresos (gasto, motivo) VALUES (%s, %s)"
-        values = (gas, mot)
-        total = (subtotal - gas)
-        eject.execute(sql, values)
-        conexion1.commit()
-        print(eject.rowcount, input("Valor insertado correctamente, pulsa cualquier tecla para continuar " + str(total)))
-        os.system("cls")
+        print("Esta es la cantidad actual con la que cuentas de dinero: " + str(total))
+        input("Pulsa cualquier tecla para continuar")
     elif opcion == "2":
-        os.system("cls")
-        print("Esta es la cantidad de dinero con la que cuentas actualmente: " + str(total))
+        print("Escribe la cantidad que hayas gastado")
+        gasto = int(input())
+        print("Ingresa el motivo del gasto")
+        motGasto = input()
+        total = total - gasto
+        print("Muy bien, con el gasto registrado ahora tu cantidad de dinero es de: " + str(total))
+        feGasto = datetime.date.today()
+        ejecucion.execute(f"INSERT INTO ingresos (gasto, motGasto, feGasto, total) VALUES (%s, %s, %s, %s)", (gasto, motGasto, feGasto, total))
+        conexion.commit()
         input("Pulsa cualquier tecla para continuar")
-        os.system("cls")
     elif opcion == "3":
-        os.system("cls")
-        ncant = input("Ingrese la nueva cantidad de dinero ")
-        total = ncant        
-        print("Esta es la cantidad de dinero con la que cuentas actualmente: " + str(total))
+        print("Escribe la cantidad que hayas ingresado")
+        ingreso = int(input())
+        print("Ingresa el motivo del ingreso")
+        motIngreso = input()
+        total = total + ingreso
+        print("Muy bien, con el ingreso registrado ahora tu cantidad de dinero es de: " + str(total))
+        feIngreso = datetime.date.today()
+        ejecucion.execute("INSERT INTO ingresos (ingreso, motIngreso, feIngreso, total) VALUES (%s, %s, %s, %s)", (ingreso, motIngreso, feIngreso, total))
+        conexion.commit()
         input("Pulsa cualquier tecla para continuar")
-        os.system("cls")
     elif opcion == "4":
-        ing = int(input("Ingrese un cantidad de dinero: "))
-        mot = input('Ingrese el motivo del gasto: ')
-        sql = "INSERT INTO ingresos (ingreso, motivo) VALUES (%s, %s)"
-        values = (ing, mot)
-        eject.execute(sql, values)
-        conexion1.commit()
-        print(eject.rowcount, "record inserted.")
+        print("Ingresa la nueva cantidad de dinero que tienes")
+        total = int(input())
+        ejecucion.execute(f"INSERT INTO ingresos (total) VALUES ({total})" )
+        conexion.commit()
+        print("Muy bien, ahora la nueva cantidad que tienes de dinero es de: " + str(total))
+        input("Pulsa cualquier tecla para continuar")
     elif opcion == "5":
-        os.system("cls")
-        print("Nos vemos pronto")
-        break
+        print("Estos son los ultimos gastos que se han hecho")
+        ejecucion.execute("SELECT gasto, motGasto, feGasto FROM ingresos ORDER BY id")
+        gasInf = ejecucion.fetchall()
+        for x in gasInf:
+            print(f"Gasto: $ {x[0]} | Motivo: {x[1]} | Fecha: {x[2]}")
+        input("Pulsa cualquier tecla para continuar")
     elif opcion == "6":
-        eject.execute("SELECT * FROM ingresos")
-        resultados = eject.fetchall()
-        for fila in resultados:
-            print(fila)
-    else:
-        print("Opción inválida. Por favor, seleccione una opción válida.")
-        input("Presiona enter para continuar")
+        print("Estos son los ultimos ingresos que se han hecho")
+        ejecucion.execute("SELECT ingreso, motIngreso, feIngreso FROM ingresos ORDER BY id")
+        gasInf = ejecucion.fetchall()
+        for x in gasInf:
+            print(f"Ingreso: $ {x[0]} | Motivo: {x[1]} | Fecha: {x[2]}")
+        input("Pulsa cualquier tecla para continuar")
+    elif opcion == "7":
+         print("¿Estás seguro de que deseas borrar todo el historial? si / no")
+         sel = input()
+         if sel == "si":
+             pass
+         elif sel == "no":
+             pass
